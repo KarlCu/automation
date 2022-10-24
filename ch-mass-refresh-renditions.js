77099db4761034c00d3bdde1bdbf7eb644705c6f
KarlCu <70559741+KarlCu@users.noreply.github.com>
2022-10-24 21:54:30 +1100

    Update ch-mass-refresh-renditions.js

--- ch-mass-refresh-renditions.js
+++ ch-mass-refresh-renditions.js
@@ -3,10 +3,21 @@
 // Sorting: Date modified - ascending (oldest to latest)
 // Filter: 
 //   Asset media: Images; Vectors
-// To execute, type in console: initAutomation({total_number_of_assets}, {number_of_seconds_before_start})
-//   Example: initAutomation(29653, 5)
+// To initiatlize execution, type in console: initAutomation({total_number_of_assets})
+//   Example: initAutomation(29653)
+// To continue interrupted execution, type in console: contAutomation()
+
+// Adjust these variables if needed:
+var minutesEachExecution = 30; // Minutes of execution per batch
+var secondsToStart = 10; // Seconds to start when page loads
+var isTestingOnly = false; // If testing only, 'Cancel' will be clicked instead of 'Submit'
+
+// Constant for Content Hub:
+var assetsPerExecution = 2000; // Maximum assets per execution, constant of 2000 assets for CH
+
 function clearSelection() {
   console.log("clearSelection()");
+
   var btn = document.querySelector('[data-testid="clear-selection"]');
   if (btn) {
     btn.click();
@@ -15,6 +26,7 @@ function clearSelection() {
 
 function refreshAssetsSelection() {
   console.log("refreshAssetsSelection()");
+
   var btn = document.querySelector('[data-testid="refresh-search"]');
   if (btn) {
     btn.click();
@@ -23,6 +35,7 @@ function refreshAssetsSelection() {
 
 function selectAllAssets() {
   console.log("selectAllAssets()");
+
   var btn = document.querySelector('[data-testid="add-all-to-selection"]');
   if (btn) {
     btn.click();
@@ -31,14 +44,20 @@ function selectAllAssets() {
 
 function openEllipsisDropdown() {
   console.log("openEllipsisDropdown()");
+
   var btn = document.querySelector('[data-testid="selection-operation-dropdown-button"]');
   if (btn) {
     btn.click();
+  }
+  else {
+    console.log("Session expired. Refreshing the page...");
+    location.reload();
   }
 }
 
 function clickRefreshRenditions() {
   console.log("clickRefreshRenditions()");
+
   var btn = document.querySelector('[data-testid="RefreshRenditions"]');
   if (btn) {
     btn.click();
@@ -47,6 +66,7 @@ function clickRefreshRenditions() {
 
 function untoggleRefreshFailedOnly() {
   console.log("untoggleRefreshFailedOnly()");
+
   var btn = document.getElementById('toggleRefreshFailedOnly');
   if (btn) {
     btn.click();
@@ -55,6 +75,7 @@ function untoggleRefreshFailedOnly() {
 
 function clickLoginWithOkta() {
   console.log("clickLoginWithOkta()");
+
   var btn = document.getElementById('SAML');
   if (btn) {
     btn.click();
@@ -63,6 +84,7 @@ function clickLoginWithOkta() {
 
 function submitSignInOkta() {
   console.log("submitSignInOkta()");
+
   var btn = document.getElementById('okta-signin-submit');
   if (btn) {
     btn.click();
@@ -71,139 +93,139 @@ function submitSignInOkta() {
 
 function submit() {
   console.log("clickRefreshRenditions()");
-  // For testing: 
-  // var btn = document.querySelector('[data-bind="click: cancel, text: messages.cancel"]');
+
   var btn = document.getElementsByClassName('btn-primary');
+
+  if (isTestingOnly) { // use 'Cancel' button for testing
+    btn = document.querySelectorAll('[data-bind="click: cancel, text: messages.cancel"]');
+  }
+
   if (btn && btn.length > 0) {
     btn[0].click();
   }
 }
 
 function isAssetsLandingPage() {
-  return window.location.pathname == "/en-us/Assets";
+  return window.location.pathname.toLowerCase() == "/en-us/assets";
 }
 
 function isRefreshRenditionsPage() {
-  return window.location.pathname.startsWith("/en-us/massedit/refreshrenditions");
+  return window.location.pathname.toLowerCase().startsWith("/en-us/massedit/refreshrenditions");
 }
 
 function isLoginPage() {
-  return window.location.pathname == "/en-us/Account";
+  return window.location.pathname.toLowerCase() == "/en-us/account";
 }
 
 function isLoginOktaPage() {
-  return window.location.pathname == "/login/login.htm";
+  return window.location.pathname.toLowerCase() == "/login/login.htm";
 }
 
-function executeRefreshRenditions(startsInSeconds) {
-
-  var startsInMilliseconds = startsInSeconds * 1000;
+function executeUIProcess() {
+  var millisecondsToStart = secondsToStart * 1000;
 
   // assets page
   if (isAssetsLandingPage()) {
     setTimeout(() => {
       clearSelection();
-    }, startsInMilliseconds);
+    }, millisecondsToStart);
 
     setTimeout(() => {
       refreshAssetsSelection();
-    }, startsInMilliseconds + 2000);
+    }, millisecondsToStart + 5000);
+
+    setTimeout(() => {
+      refreshAssetsSelection(); // re-execute refresh
+    }, millisecondsToStart + 10000);
 
     setTimeout(() => {
       selectAllAssets();
-    }, startsInMilliseconds + 8000);
+    }, millisecondsToStart + 15000);
 
     setTimeout(() => {
       openEllipsisDropdown();
-    }, startsInMilliseconds + 14000);
+    }, millisecondsToStart + 20000);
 
     setTimeout(() => {
       clickRefreshRenditions();
-    }, startsInMilliseconds + 16000);
+    }, millisecondsToStart + 22000);
   }
 
   // redirects to refresh renditions page
   if (isRefreshRenditionsPage()) {
     setTimeout(() => {
       untoggleRefreshFailedOnly();
-    }, startsInMilliseconds);
+    }, millisecondsToStart);
 
     setTimeout(() => {
       submit();
-    }, startsInMilliseconds + 2000);
+    }, millisecondsToStart + 5000);
   }
 
   // if session times out and redirects to login
   if (isLoginPage()) {
     setTimeout(() => {
-      clickLoginWithOkta()();
-    }, startsInMilliseconds);
+      clickLoginWithOkta();
+    }, millisecondsToStart);
   }
 
   if (isLoginOktaPage()) {
     setTimeout(() => {
-      submitSignInOkta()();
-    }, startsInMilliseconds);
+      submitSignInOkta();
+    }, millisecondsToStart);
   }
 }
 
 var keyTotalAssets = "ch-refresh-renditions-total-assets";
 var keyCounter = "ch-refresh-renditions-counter";
-var keyStartsInSeconds = "ch-refresh-renditions-starts-in-seconds";
 
-function runAutomation(totalAssets, startsInSeconds) {
-  var totalExecution = Math.ceil(totalAssets / 2000) + 1; // add extra 1
-
-  if (!startsInSeconds) {
-    startsInSeconds = 1;
-  }
-
+function initAutomation(totalAssets) {
+  console.log("Initialize...");
   sessionStorage.setItem(keyTotalAssets, totalAssets);
-  sessionStorage.setItem(keyStartsInSeconds, startsInSeconds);
+  sessionStorage.setItem(keyCounter, 0);
+  executeUIProcess();
+  return true;
+}
 
+function contAutomation() {
+  var totalAssets = sessionStorage.getItem(keyTotalAssets);
+  var totalExecution = Math.ceil(totalAssets / assetsPerExecution) + 1; // add extra 1
   var counter = sessionStorage.getItem(keyCounter);
-  console.log("Total Execution: " + totalExecution + "; Executed: " + counter + ";");
 
   if (counter) {
+    console.log("Batches: " + totalExecution + "; Executed: " + counter + ";");
+
     if (counter < totalExecution) {
       if (isAssetsLandingPage()) {
+        var millisecondsEachExecution = minutesEachExecution * 1000 * 60;
+
         setTimeout(() => {
-          executeRefreshRenditions(startsInSeconds);
-        }, 1800000); // set to 30 mins each execution (1800000 milliseconds)
+          executeUIProcess();
+        }, millisecondsEachExecution);
       }
-      else {
-        executeRefreshRenditions(startsInSeconds);
+	  else {
+        executeUIProcess();
 
         if (isRefreshRenditionsPage()) {
           sessionStorage.setItem(keyCounter, Number(counter) + 1);
         }
       }
-    } 
-    else {
-      console.log("Execution completed!");
     }
-  } 
-  else { // initial execution
-    if (isAssetsLandingPage()) {
-      console.log("Initial execution...");
-      sessionStorage.setItem(keyCounter, 0);
-      executeRefreshRenditions(startsInSeconds);
+	else {
+      console.log("Completed!");
+      sessionStorage.removeItem(keyTotalAssets);
+      sessionStorage.removeItem(keyCounter);
     }
   }
+  else {
+    if (isLoginPage() || isLoginOktaPage()) {
+      executeUIProcess();
+    }
+  }
+
+  return true;
 }
 
 window.addEventListener('load', function(event) {
-  var totalAssets = sessionStorage.getItem(keyTotalAssets);
-  var startsInSeconds = sessionStorage.getItem(keyStartsInSeconds);
-
-  if (totalAssets > 0) {
-    runAutomation(totalAssets, startsInSeconds);
-  }
+  contAutomation();
 });
-
-function initAutomation(totalAssets, startsInSeconds) {
-  sessionStorage.removeItem(keyTotalAssets);
-  sessionStorage.removeItem(keyCounter);
-  sessionStorage.removeItem(keyStartsInSeconds);
-  runAutomation(totalAssets, startsInSeconds);
-}
